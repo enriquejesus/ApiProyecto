1d0a4c417c905fb43c8c8d97fa5b919a2d9270fb
enrique <user23qiqe@gmail.com>
2020-08-09 20:05:22 -0500

    Agrega método obtenerEscuelas

---
+++ models/escuelasModel.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once 'db.php'; //Se incluye el archivo de la conexión a la BD
+
+    class EscuelasModel extends DB{ //Se inicia la clase EscuelasModel trayendo todos los datos de BD
+
+        function obtenerEscuelas(){ //Se crea la función obtenerEscuelas
+            $query = $this->connect()->query('SELECT * FROM escuelas'); //Se realiza el query 
+            
+            return $query; //Se regresa el query
+        }//finaliza la función
+    }//Finaliza la clase EscuelasModel
+?>
